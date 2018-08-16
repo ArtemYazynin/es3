852341ec7573fd9331e238e10d9cae2b614e508b
ArtemYazynin <yazynindev@gmail.com>
2018-08-16 18:21:12 +0400

    implementin address component;

--- src/app/shared/address.service.ts
+++ src/app/shared/address.service.ts
@@ -15,27 +15,77 @@ export class AddressService {
   constructor(private http: HttpInterceptor, private jsonp: Jsonp) { }
 
   getRegions(searchStr: string): Observable<Array<Location>> {
-    if (isNullOrUndefined(searchStr) || searchStr.trim() == "") return of([]);
+    if (this.invalid(searchStr)) return this.getDefault();
     const urlTail = this.urlBuilder({
       contentType: "region",
-      query: encodeURI(searchStr.trim()) 
+      query: encodeURI(searchStr.trim())
     });
     const url = this.url.concat(urlTail);
     return this.sendRequest(url);
   }
-  getCities(regionId: string, searchStr: string): Observable<Array<Location>> {
-    if (isNullOrUndefined(searchStr) || searchStr.trim() == "") return of([]);
-    const urlTail = this.urlBuilder({
-      regionId: regionId,
-      parentId: regionId,
-      parentType: "region",
+  getCities(location: Location, searchStr: string): Observable<Array<Location>> {
+    if (isNullOrUndefined(location) || this.invalid(searchStr)) return this.getDefault();
+    const dynamicParams = (() => {
+      let result = {
+        parentType: location.contentType
+      };
+      result[location.contentType == "district" ? "districtId" : "regionId"] = location.id
+      return result;
+    })();
+    const params = Object.assign({}, dynamicParams, {
+      parentId: location.id,
       contentType: "city",
-      query: encodeURI(searchStr.trim()) 
+      query: encodeURI(searchStr.trim())
+    });
+    const urlTail = this.urlBuilder(params);
+    const url = this.url.concat(urlTail);
+    return this.sendRequest(url);
+  }
+
+  getDistricts(region: Location, searchStr: string) {
+    if (isNullOrUndefined(region) || this.invalid(searchStr)) return this.getDefault();
+    const urlTail = this.urlBuilder({
+      regionId: region.id,
+      parentId: region.id,
+      parentType: "region",
+      contentType: "district",
+      query: encodeURI(searchStr.trim())
+    });
+    const url = this.url.concat(urlTail);
+    return this.sendRequest(url);
+  }
+  getStreets(city: Location, searchStr: string) {
+    if (isNullOrUndefined(city) || this.invalid(searchStr)) return this.getDefault();
+    const urlTail = this.urlBuilder({
+      cityId: city.id,
+      parentId: city.id,
+      parentType: "city",
+      contentType: "street",
+      query: encodeURI(searchStr.trim())
+    });
+    const url = this.url.concat(urlTail);
+    return this.sendRequest(url);
+  }
+  getBuildings(street: Location, searchStr: string) {
+    if (isNullOrUndefined(street) || this.invalid(searchStr)) return this.getDefault();
+    const urlTail = this.urlBuilder({
+      streetId: street.id,
+      parentId: street.id,
+      parentType: "street",
+      contentType: "building",
+      query: encodeURI(searchStr.trim())
     });
     const url = this.url.concat(urlTail);
     return this.sendRequest(url);
   }
 
+  private invalid(searchStr: string) {
+    if ((typeof searchStr) != "string") return true;
+    return isNullOrUndefined(searchStr) || searchStr.trim() == ""
+  }
+  private getDefault() {
+    return of([]);
+  }
   private sendRequest(url: string): Observable<Array<Location>> {
     return this.jsonp.request(url).pipe(map((response: Response) => {
       let result = <Array<Location>>response.json()["result"];
--- src/app/shared/address/address.component.ts
+++ src/app/shared/address/address.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, FormControl, AbstractControl } from '@angular/forms';
 import { Observable, Subject, pipe } from 'rxjs';
 import { debounceTime, takeUntil, filter, distinctUntilChanged } from 'rxjs/operators';
 import { AddressService, Location } from '..';
 import { MatAutocompleteSelectedEvent, MatSelectChange } from '@angular/material';
+import { isNullOrUndefined } from 'util';
 
 @Component({
   selector: 'app-address',
@@ -14,19 +15,67 @@ export class AddressComponent implements OnInit, OnDestroy {
 
   private ngUnsubscribe: Subject<any> = new Subject();
   addressForm: FormGroup;
-  private debounceTime = 700;
+  private debounceTime = 300;
   regionChildTypes = {
     district: 0,
     city: 1
   }
   regions: Observable<Array<Location>>;
   cities: Observable<Array<Location>>;
+  districts: Observable<Array<Location>>;
+  streets: Observable<Array<Location>>;
+  buildings: Observable<Array<Location>>;
   constructor(private addressService: AddressService, private fb: FormBuilder) { }
 
   ngOnDestroy(): void {
     this.ngUnsubscribe.next();
     this.ngUnsubscribe.complete();
   }
+
+  selected = {
+    region: () => {
+      return (typeof this.addressForm.controls.region.value) == "object";
+    },
+    districtType: () => {
+      if (this.addressForm.controls.regionChildType.value == "") return false;
+      return this.addressForm.controls.regionChildType.value == this.regionChildTypes.district;
+    },
+    cityTypeOrDistrict: () => {
+      return this.addressForm.controls.regionChildType.value == this.regionChildTypes.city
+        || (typeof this.addressForm.controls.district.value) == "object";
+    },
+    city: () => {
+      return (typeof this.addressForm.controls.city.value) == "object";
+    },
+    street: () => {
+      return (typeof this.addressForm.controls.street.value) == "object";
+    },
+  }
+
+  placeHolder = (() => {
+    const get = (control: AbstractControl, defaultValue: string) => {
+      return (typeof control.value) == "object"
+        ? control.value.type
+        : defaultValue;
+    }
+    return {
+      region: () => {
+        return get(this.addressForm.controls.region, "Регион");
+      },
+      district: () => {
+        return get(this.addressForm.controls.district, "Район");
+      },
+      city: () => {
+        return get(this.addressForm.controls.city, "Город");
+      },
+      street: () => {
+        return get(this.addressForm.controls.street, "Улица");
+      },
+      building: () => {
+        return get(this.addressForm.controls.building, "Дом");
+      },
+    }
+  })();
 
   ngOnInit() {
     this.buildForm();
@@ -45,33 +94,95 @@ export class AddressComponent implements OnInit, OnDestroy {
         distinctUntilChanged(),
         debounceTime(this.debounceTime))
       .subscribe(value => {
-        this.cities = this.addressService.getCities(this.addressForm.controls.region.value,value);
+        const control = (typeof this.addressForm.controls.district.value) == "object"
+          ? this.addressForm.controls.district.value
+          : this.addressForm.controls.region.value
+        this.cities = this.addressService.getCities(control, value);
+      });
+    this.addressForm.controls.district.valueChanges
+      .pipe(
+        takeUntil(this.ngUnsubscribe),
+        distinctUntilChanged(),
+        debounceTime(this.debounceTime))
+      .subscribe(value => {
+        this.districts = this.addressService.getDistricts(this.addressForm.controls.region.value, value);
+      });
+    this.addressForm.controls.street.valueChanges
+      .pipe(
+        takeUntil(this.ngUnsubscribe),
+        distinctUntilChanged(),
+        debounceTime(this.debounceTime))
+      .subscribe(value => {
+        this.streets = this.addressService.getStreets(this.addressForm.controls.city.value, value);
+      });
+    this.addressForm.controls.building.valueChanges
+      .pipe(
+        takeUntil(this.ngUnsubscribe),
+        distinctUntilChanged(),
+        debounceTime(this.debounceTime))
+      .subscribe(value => {
+        this.buildings = this.addressService.getBuildings(this.addressForm.controls.street.value, value);
       });
   }
-  displayFn(entity?: Location): string | undefined {
-    return entity ? (entity.fullName || entity.name) : undefined;
+  display = {
+    region: (entity?: Location) => {
+      return entity.fullName || entity.name
+    },
+    other: (entity?: Location) => {
+      return entity
+        ? entity.typeShort ? (entity.typeShort + ".").concat(entity.name) : ""
+        : undefined;
+    }
   }
   onChange = (() => {
-    const region = (e: MatAutocompleteSelectedEvent) => {
+    const clearForm = (form: FormGroup, except: string) => {
+      if (isNullOrUndefined(form)
+        || isNullOrUndefined(except)
+        || except == "") return;
+      except = except.trim();
+      for (const key in form.controls) {
+        if (form.controls.hasOwnProperty(key)) {
+          if (key == except) continue;
+          form.controls[key].patchValue("");
+        }
+      }
+    }
+    const region = (context: AddressComponent, e: MatAutocompleteSelectedEvent) => {
       if (!!e.option.value == false) return;
-      const location = <Location>e.option.value;
+      clearForm(context.addressForm, "region");
     }
-    const city = (e: MatAutocompleteSelectedEvent) => {
+    const city = (context: AddressComponent, e: MatAutocompleteSelectedEvent) => {
+      clearForm(context.addressForm, "city");
+    }
+    const district = (context: AddressComponent, e: MatAutocompleteSelectedEvent) => {
 
     }
-    const regionChildType = (e: MatSelectChange) => {
-
+    const regionChildType = (context: AddressComponent, e: MatSelectChange) => {
 
     }
+    const street = (context: AddressComponent, e: MatSelectChange) => {
+
+    }
+    const building = (context: AddressComponent, e: MatSelectChange) => {
+
+    }
+
     return {
       region: region,
       regionChildType: regionChildType,
-      city: city
+      city: city,
+      district: district,
+      street: street,
+      building: building
     }
   })();
   private buildForm() {
     this.addressForm = this.fb.group({
       "region": [
+        "",
+        []
+      ],
+      "regionChildType": [
         "",
         []
       ],
@@ -91,14 +202,9 @@ export class AddressComponent implements OnInit, OnDestroy {
         "",
         []
       ],
-      "regionChildType": [
-        "",
-        []
-      ],
+
     })
   }
 
-  hasRegion() {
-    return (typeof this.addressForm.controls.region.value) == "object";
-  }
+
 }
