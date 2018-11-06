e67f80d524e162444348319fbf0ae1bc15d6d992
VladimirAbizov <abizov-v5@rambler.ru>
2018-11-06 12:18:41 +0400

    fix bug #37 output type current education place https://github.com/ArtemYazynin/es3/issues/37

--- src/app/shared/components/current-education-place-card/current-education-place-card.component.ts
+++ src/app/shared/components/current-education-place-card/current-education-place-card.component.ts
@@ -1,6 +1,5 @@
-import { Component, Input, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
 import { Entity, InstitutionService, SpecHealth } from '../..';
 import { CurrentEducationPlace } from '../../../modules/wizard/shared';
 
@@ -14,13 +13,12 @@ export class CurrentEducationPlaceCardComponent implements OnInit {
   @Input() edit: () => void;
   @Input() currentEducationPlace: CurrentEducationPlace;
 
-  $institutionType: Observable<Entity<number>>
+  $institutionType: Observable<Entity<number>[]>
   $specHealth: Observable<SpecHealth>
 
   constructor(private institutionService: InstitutionService) { }
 
   ngOnInit() {
-    this.$institutionType = this.institutionService.getTypes(this.currentEducationPlace.institutionType)
-      .pipe(map(types => types[0]));
+    this.$institutionType = this.institutionService.getTypes(this.currentEducationPlace.institutionType);
   }
 }
