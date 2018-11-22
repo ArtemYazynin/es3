1ecade41494f317f55faf8d6453ceed664a6ee84
CORP\Artem <yazynindev@gmail.com>
2018-11-22 18:22:16 +0400

    privilegeStepRedesign

--- src/app/modules/wizard/privilege-step/privilege-step.component.ts
+++ src/app/modules/wizard/privilege-step/privilege-step.component.ts
@@ -1,37 +1,33 @@
-import { AfterViewInit, ChangeDetectionStrategy, ChangeDetectorRef, Component, OnInit, ViewChild } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit, ViewChild } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { ButtonsTitles, ConfigsOfRoutingButtons, Inquiry } from '../../../shared';
 import { ActionsButtonsService } from '../../../shared/actions-buttons.service';
 import { EditPrivilegeComponent } from '../../../shared/components/edit-privilege/edit-privilege.component';
-import { StepBase, WizardStorageService } from '../shared';
+import { WizardStorageService } from '../shared';
 
 @Component({
   selector: 'app-privilege-step',
-  providers:[ActionsButtonsService],
+  providers: [ActionsButtonsService],
   templateUrl: './privilege-step.component.html',
   styleUrls: ['./privilege-step.component.css'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class PrivilegeStepComponent implements OnInit, AfterViewInit, StepBase {
+export class PrivilegeStepComponent implements OnInit {
   @ViewChild(EditPrivilegeComponent) privilegeEditComponent: EditPrivilegeComponent;
   inquiry: Inquiry;
   inquiryType = this.route.snapshot.data.resolved.inquiryType;
   config: ConfigsOfRoutingButtons;
 
   constructor(private storageService: WizardStorageService, private router: Router, private activatedRoute: ActivatedRoute,
-    private route: ActivatedRoute, private cdr: ChangeDetectorRef, private actionsButtonsService: ActionsButtonsService) {
+    private route: ActivatedRoute, private actionsButtonsService: ActionsButtonsService) {
   }
 
   ngOnInit() {
     this.inquiry = <Inquiry>this.storageService.get(this.inquiryType);
-  this.config = new ConfigsOfRoutingButtons(ButtonsTitles.Next, ButtonsTitles.Back,
+    this.config = new ConfigsOfRoutingButtons(ButtonsTitles.Next, ButtonsTitles.Back,
       this.actionsButtonsService.primaryActionPrivilegeStep(this.privilegeEditComponent, this.inquiry, this.activatedRoute),
       this.actionsButtonsService.inverseActionPrivilegeStep(this.inquiryType, this.router, this.activatedRoute)
     );
-  }
-
-  ngAfterViewInit(): void {
-    this.cdr.detectChanges();
   }
 
   isValid() {
