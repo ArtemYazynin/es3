e88327443d6afda9c0ba605656e83bb1cd544963
CORP\Artem <yazynindev@gmail.com>
2019-01-16 14:51:53 +0400

    accept create Inquiry in httpInterceptor

--- src/app/shared/Es3Interceptor.ts
+++ src/app/shared/Es3Interceptor.ts
@@ -1,11 +1,12 @@
 import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent, HttpResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
+import { Inquiry } from './models/inquiry.model';
 
 @Injectable()
 export class Es3Interceptor implements HttpInterceptor {
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-        if (req.method === "PUT" || req.method === "POST") {
+        if (!(req.body instanceof Inquiry) && (req.method === "PUT" || req.method === "POST")) {
             return of(new HttpResponse({
                 body: req.body
             }));
