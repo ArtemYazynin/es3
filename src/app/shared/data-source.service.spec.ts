57e279583bce89599d1b4a4ee82c433879962e77
CORP\Artem <yazynindev@gmail.com>
2019-01-17 16:33:23 +0400

    dataSource spec refactoring

--- src/app/shared/data-source.service.spec.ts
+++ src/app/shared/data-source.service.spec.ts
@@ -1,24 +1,23 @@
-import { TestBed, async } from '@angular/core/testing';
-
-import { DataSourceService } from './data-source.service';
-import { Inquiry } from './models/inquiry.model';
-import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
-import { SERVER_URL } from '../app.module';
 import { HttpClient } from '@angular/common/http';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { Injector } from '@angular/core';
+import { async, TestBed, getTestBed } from '@angular/core/testing';
+import { SERVER_URL } from '../app.module';
+import { DataSourceService } from './data-source.service';
+
 
 class Mock {
-  constructor(public id:string, public name:string){}
+  constructor(public id: string, public name: string) { }
 }
 
 fdescribe('InquiryDataSourceService', () => {
   let http: HttpTestingController;
   let service: DataSourceService<Mock>;
   let mocks = [
-    new Mock("0","Иванов"),
-    new Mock("1","Петров"),
-    new Mock("2","Сидоров"),
-    new Mock("3","Щукин"),
+    new Mock("0", "Иванов"),
+    new Mock("1", "Петров"),
+    new Mock("2", "Сидоров"),
+    new Mock("3", "Щукин"),
   ];
   let mock = mocks[0];
   beforeEach(async(() => {
@@ -31,7 +30,7 @@ fdescribe('InquiryDataSourceService', () => {
         { provide: SERVER_URL, useValue: "http://localhost:3500" }
       ]
     });
-    service = new DataSourceService<Mock>(TestBed.get(HttpClient), TestBed.get(Injector),"mocks");
+    service = new DataSourceService<Mock>(TestBed.get(HttpClient), TestBed.get(Injector), "mocks");
     http = TestBed.get(HttpTestingController);
   }));
 
@@ -44,33 +43,45 @@ fdescribe('InquiryDataSourceService', () => {
     expect(service).toBeTruthy();
   });
 
-  it("gets", ()=>{
-    service.gets().subscribe(response=>{
-      expect(response.length).toEqual(mocks.length);
-    },fail);
-    const req = http.expectOne(service.api);
-    expect(req.request.method).toEqual('GET');
-    req.flush(mocks);
+  it("gets", () => {
+    gets(mocks, undefined);
   });
 
-  it("gets with queryParams", ()=>{
+  it("gets with queryParams", () => {
     const index = 2;
     const queryParams = `name=${mocks[index].name}`;
-    service.gets(queryParams).subscribe(response=>{
-      expect(response.length).toEqual(mocks.filter(x=>x.name == mocks[index].name).length);
-    },fail);
-    const req = http.expectOne(`${service.api}?${queryParams}`);
-    expect(req.request.method).toEqual('GET');
-    req.flush(mocks.filter(x=>x.name == mocks[index].name));
+    gets(mocks.filter(x => x.name == mocks[index].name), queryParams);   
   });
 
-  it("gets will be return empty array", ()=>{
-    service.gets().subscribe(response=>{
-      expect(response.length).toEqual(0);
-    },fail);
-    const req = http.expectOne(service.api);
-    expect(req.request.method).toEqual('GET');
-    req.flush([]);
+  it("gets will be return empty array", () => {
+    gets([], undefined)
   });
 
+  it("get will return one of the mock", () => {
+    get(mock.id, mock);
+  });
+  it("get will return null. Not existing ID", () => {
+    let invalidId = (mocks.length + 1).toString();
+    get(invalidId, null);
+  });
+
+  let gets = (expectedMocks:Mock[], queryParams) => {
+    service.gets(queryParams).subscribe(response => {
+      expect(response.length).toEqual(expectedMocks.length);
+    }, fail);
+    const req = http.expectOne(queryParams ? `${service.api}?${queryParams}` : service.api);
+    expect(req.request.method).toEqual('GET');
+    req.flush(expectedMocks);
+  }
+
+  let get = (id, expectedMock) => {
+    service.get(id).subscribe(response => {
+      expect(response).toEqual(expectedMock);
+    }, fail);
+    const expectedUrl = `${service.api}/${id}`
+    const req = http.expectOne(expectedUrl);
+    expect(req.request.url).toEqual(expectedUrl);
+    expect(req.request.method).toEqual('GET');
+    req.flush(expectedMock);
+  };
 });
