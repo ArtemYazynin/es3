d7933e28f8893af740ce56be8cfcbd59a167ea22
CORP\Artem <yazynindev@gmail.com>
2019-01-21 11:35:40 +0400

    gender component test;

--- src/app/shared/components/gender/gender.component.spec.ts
+++ src/app/shared/components/gender/gender.component.spec.ts
@@ -1,13 +1,18 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { GenderComponent } from './gender.component';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MaterialModule } from '../../../material.module';
 
-describe('GenderComponent', () => {
+fdescribe('GenderComponent', () => {
   let component: GenderComponent;
   let fixture: ComponentFixture<GenderComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      schemas:[NO_ERRORS_SCHEMA],
+      //imports: [FormsModule, MaterialModule],
       declarations: [ GenderComponent ]
     })
     .compileComponents();
@@ -21,5 +26,8 @@ describe('GenderComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+  it('should initialize gender property value', () => {
+    expect(component.gender).toEqual(1);
   });
 });
