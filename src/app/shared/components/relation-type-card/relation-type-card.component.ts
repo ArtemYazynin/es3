8d1a02f3321984485bc898c44c4d544e1e93c58f
VladimirAbizov <abizov-v5@rambler.ru>
2019-01-23 00:32:31 +0400

    partial realize tests for RelationTypeDialog

--- src/app/shared/components/relation-type-card/relation-type-card.component.ts
+++ src/app/shared/components/relation-type-card/relation-type-card.component.ts
@@ -1,17 +1,17 @@
-import { Component, OnInit, ChangeDetectionStrategy, Input, ChangeDetectorRef, OnDestroy } from '@angular/core';
-import { Parent, CommonService, InquiryService, ConfirmationDocument, BehaviorMode, Theme } from '../..';
-import { Subject, BehaviorSubject } from 'rxjs';
-import { skip, takeUntil } from 'rxjs/operators';
-import { ActivatedRoute } from '@angular/router';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, Input, OnDestroy, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material';
-import { PersonService } from '../../person.service';
+import { ActivatedRoute } from '@angular/router';
+import { BehaviorSubject, Subject } from 'rxjs';
+import { skip, takeUntil } from 'rxjs/operators';
+import { BehaviorMode, CommonService, ConfirmationDocument, InquiryService, Parent, Theme } from '../..';
 import { RelationTypeDialogComponent } from '../../../modules/inquiry/relation-type-dialog/relation-type-dialog.component';
+import { PersonService } from '../../person.service';
 
 @Component({
   selector: 'app-relation-type-card',
   templateUrl: './relation-type-card.component.html',
   styleUrls: ['./relation-type-card.component.css'],
-  host:{ 'class': 'host'},
+  host: { 'class': 'host' },
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class RelationTypeCardComponent implements OnInit, OnDestroy {
@@ -22,7 +22,7 @@ export class RelationTypeCardComponent implements OnInit, OnDestroy {
   title = "Родственная связь";
   modes = BehaviorMode;
   themes = Theme;
-  theme:Theme;
+  theme: Theme;
   $parentRepresentChildrenDocument: BehaviorSubject<ConfirmationDocument>;
 
   constructor(private route: ActivatedRoute, private dialog: MatDialog, private cdr: ChangeDetectorRef, private commonService: CommonService,
@@ -46,8 +46,8 @@ export class RelationTypeCardComponent implements OnInit, OnDestroy {
   }
 
   edit() {
-    let config = { $parent: new BehaviorSubject<Parent>(this.model) };
-    config.$parent
+    let config = { $owner: new BehaviorSubject<Parent>(this.model) };
+    config.$owner
       .pipe(skip(1), takeUntil(this.ngUnsubscribe))
       .subscribe((person: Parent) => {
         this.personService.update(person.id, person).subscribe((newPerson: Parent) => {
