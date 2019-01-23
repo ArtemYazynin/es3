ceff808a5d0946b75cee90b728ceb4f3f056b9ee
VladimirAbizov <abizov-v5@rambler.ru>
2019-01-23 13:27:14 +0400

    partial realize tests for RelationTypeDialog (inverse/primary)

--- src/app/modules/inquiry/relation-type-dialog/relation-type-dialog.component.spec.ts
+++ src/app/modules/inquiry/relation-type-dialog/relation-type-dialog.component.spec.ts
@@ -4,7 +4,7 @@ import { MatDialog, MatDialogModule, MatDialogRef, MAT_DIALOG_DATA } from '@angu
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { BehaviorSubject } from 'rxjs';
 import { SERVER_URL } from '../../../app.module';
-import { Parent } from '../../../shared';
+import { Parent, RelationType } from '../../../shared';
 import { DialogButtonsComponent } from '../dialog-buttons/dialog-buttons.component';
 import { InquiryModule } from '../inquiry.module';
 import { RelationTypeDialogComponent } from './relation-type-dialog.component';
@@ -13,7 +13,11 @@ fdescribe('RelationTypeDialogComponent', () => {
   let component: RelationTypeDialogComponent;
   let fixture: ComponentFixture<RelationTypeDialogComponent>;
   const mockDialogRef = { close: jasmine.createSpy('close') };
-  const defaultMatDialogData = { $owner: new BehaviorSubject<Parent>(new Parent("q", "w", "e", "123", false, undefined, undefined, 1)) };
+
+  let mockParent = new Parent("q", "w", "e", "123", false, undefined, undefined, 1);
+  mockParent.relationType = new RelationType("26B732C2-580D-4FCC-8034-A88F01009735", "Отец", false);
+
+  const defaultMatDialogData = { $owner: new BehaviorSubject<Parent>(mockParent) };
 
   let prepare = (matDialogData: { $owner: BehaviorSubject<Parent> }) => {
     TestBed.configureTestingModule({
@@ -48,5 +52,20 @@ fdescribe('RelationTypeDialogComponent', () => {
   it('relationTypeComponent.owner. Should have owner', () => {
     prepare(defaultMatDialogData);
     expect(component.relationTypeComponent.owner).toEqual(defaultMatDialogData.$owner.getValue());
+  });
+
+  it("config.inverseAction. Should call close dialog", () => {
+    prepare(defaultMatDialogData);
+    component.config.inverseAction();
+    expect(mockDialogRef.close).toHaveBeenCalled();
+  });
+
+  it("config.primaryAction. Should call: close dialog, next with default parameter.", () => {
+    prepare(defaultMatDialogData);
+    spyOn(component.data.$owner, "next");
+    component.config.primaryAction();
+
+    expect(component.data.$owner.next).toHaveBeenCalledWith(defaultMatDialogData.$owner.getValue());
+    expect(mockDialogRef.close).toHaveBeenCalled();
   });
 });
