52fe91826d324dc4ce2ad28c527c314506b5f780
VladimirAbizov <abizov-v5@rambler.ru>
2019-01-29 17:14:51 +0400

    partial realize tests for Privilege-, SpecHealth- and SchoolInquiryInfoDialogComponent

--- src/app/modules/inquiry/privilege-dialog/privilege-dialog.component.spec.ts
+++ src/app/modules/inquiry/privilege-dialog/privilege-dialog.component.spec.ts
@@ -1,24 +1,48 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { MatDialog, MatDialogModule, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { BehaviorSubject } from 'rxjs';
+import { esConstant, SERVER_URL } from '../../../app.module';
+import { Privilege } from '../../../shared';
+import { InquiryModule } from '../inquiry.module';
 import { PrivilegeDialogComponent } from './privilege-dialog.component';
+
 
 describe('PrivilegeDialogComponent', () => {
   let component: PrivilegeDialogComponent;
   let fixture: ComponentFixture<PrivilegeDialogComponent>;
+  const mockDialogRef = { close: jasmine.createSpy('close') };
 
-  beforeEach(async(() => {
+  const defaultMatDialogData = {
+    $privilege: new BehaviorSubject<Privilege>(new Privilege())
+  };
+
+  let prepare = (matDialogData: { $privilege: BehaviorSubject<Privilege> }) => {
     TestBed.configureTestingModule({
-      declarations: [ PrivilegeDialogComponent ]
-    })
-    .compileComponents();
-  }));
-
-  beforeEach(() => {
+      imports: [NoopAnimationsModule, MatDialogModule, InquiryModule, HttpClientTestingModule],
+      declarations: [],
+      providers: [
+        MatDialog,
+        {
+          provide: MatDialogRef,
+          useValue: mockDialogRef
+        },
+        {
+          provide: MAT_DIALOG_DATA,
+          useValue: matDialogData
+        },
+        { provide: SERVER_URL, useValue: "http://localhost:3500" },
+        { provide: esConstant, useValue: {} }
+      ]
+    }).compileComponents();
     fixture = TestBed.createComponent(PrivilegeDialogComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
-  });
+  }
 
   it('should create', () => {
+    prepare(defaultMatDialogData);
     expect(component).toBeTruthy();
   });
 });
--- src/app/modules/inquiry/school-inquiry-info-dialog/school-inquiry-info-dialog.component.spec.ts
+++ src/app/modules/inquiry/school-inquiry-info-dialog/school-inquiry-info-dialog.component.spec.ts
@@ -1,25 +1,47 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { MatDialog, MatDialogModule, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { BehaviorSubject } from 'rxjs';
+import { esConstant, SERVER_URL } from '../../../app.module';
+import { SchoolInquiryInfo } from '../../../shared';
+import { InquiryModule } from '../inquiry.module';
 import { SchoolInquiryInfoDialogComponent } from './school-inquiry-info-dialog.component';
 
-describe('SchoolInquiryInfoDialogComponent', () => {
+fdescribe('SchoolInquiryInfoDialogComponent', () => {
   let component: SchoolInquiryInfoDialogComponent;
   let fixture: ComponentFixture<SchoolInquiryInfoDialogComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ SchoolInquiryInfoDialogComponent ]
-    })
-    .compileComponents();
-  }));
+  const mockDialogRef = { close: jasmine.createSpy('close') };
 
-  beforeEach(() => {
+  const defaultMatDialogData = {
+    $schoolInquiryInfo: new BehaviorSubject<SchoolInquiryInfo>(undefined)
+  };
+  let prepare = (matDialogData: { $schoolInquiryInfo: BehaviorSubject<SchoolInquiryInfo> }) => {
+    TestBed.configureTestingModule({
+      imports: [NoopAnimationsModule, MatDialogModule, InquiryModule, HttpClientTestingModule],
+      declarations: [],
+      providers: [
+        MatDialog,
+        {
+          provide: MatDialogRef,
+          useValue: mockDialogRef
+        },
+        {
+          provide: MAT_DIALOG_DATA,
+          useValue: matDialogData
+        },
+        { provide: SERVER_URL, useValue: "http://localhost:3500" },
+        { provide: esConstant, useValue: {} }
+      ]
+    }).compileComponents();
     fixture = TestBed.createComponent(SchoolInquiryInfoDialogComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
-  });
+  }
 
   it('should create', () => {
+    prepare(defaultMatDialogData);
     expect(component).toBeTruthy();
   });
 });
