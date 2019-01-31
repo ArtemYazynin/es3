808c2ec3d3d070c33fbaf6353952502775211758
VladimirAbizov <abizov-v5@rambler.ru>
2019-01-31 18:03:10 +0400

    disability.component.spec in progress

--- src/app/shared/components/disability/disability.component.spec.ts
+++ src/app/shared/components/disability/disability.component.spec.ts
@@ -1,16 +1,25 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { FormsModule } from '@angular/forms';
+import { MatCheckboxModule, MatSelectModule } from '@angular/material';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { esConstant, SERVER_URL } from '../../../app.module';
 import { DisabilityComponent } from './disability.component';
 
-describe('DisabilityComponent', () => {
+fdescribe('DisabilityComponent', () => {
   let component: DisabilityComponent;
   let fixture: ComponentFixture<DisabilityComponent>;
 
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ DisabilityComponent ]
+      imports: [FormsModule, MatCheckboxModule, HttpClientTestingModule, NoopAnimationsModule, MatSelectModule],
+      providers: [
+        { provide: SERVER_URL, useValue: "http://localhost:3500" },
+        { provide: esConstant, useValue: {} }
+      ],
+      declarations: [DisabilityComponent],
     })
-    .compileComponents();
   }));
 
   beforeEach(() => {
