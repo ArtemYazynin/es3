1818dd459e9644c1b5dea221d094060bb42c0150
VladimirAbizov <abizov-v5@rambler.ru>
2019-02-05 13:03:26 +0400

    partial realize tests for SpecHealthComponent

--- src/app/shared/components/spec-health/spec-health.component.spec.ts
+++ src/app/shared/components/spec-health/spec-health.component.spec.ts
@@ -1,16 +1,30 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { FormsModule } from '@angular/forms';
+import { MatSelectModule } from '@angular/material';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { SpecHealthService } from '../..';
+import { esConstant, SERVER_URL } from '../../../app.module';
+import { MaterialModule } from '../../../material.module';
+import { CommonService } from '../../common.service';
+import { InquiryService } from '../../inquiry.service';
 import { SpecHealthComponent } from './spec-health.component';
 
-describe('SpecHealthComponent', () => {
+fdescribe('SpecHealthComponent', () => {
   let component: SpecHealthComponent;
   let fixture: ComponentFixture<SpecHealthComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ SpecHealthComponent ]
+      schemas: [NO_ERRORS_SCHEMA],
+      imports: [FormsModule, MaterialModule, HttpClientTestingModule, NoopAnimationsModule, MatSelectModule],
+      providers: [SpecHealthService, InquiryService, CommonService,
+        { provide: SERVER_URL, useValue: "http://localhost:3500" },
+        { provide: esConstant, useValue: {} },],
+      declarations: [SpecHealthComponent]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
