f2336e81ce6205b803b125ffee20408a0844a325
CORP\Artem <yazynindev@gmail.com>
2019-02-11 13:13:49 +0400

    create app loader;

--- src/app/shared/disability.service.ts
+++ src/app/shared/disability.service.ts
@@ -1,19 +1,20 @@
-import { Injectable, Inject } from '@angular/core';
+import { Injectable, Inject, Injector } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { SERVER_URL } from '../app.module';
 import { DisabilityType } from './models/disability-type.model';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { Es3HttpClient } from './es3-http-client';
+import { DataSourceService } from './data-source.service';
 
 @Injectable()
 export class DisabilityService {
-  private api = `${this.serverUrl}/disabilities`;
-  constructor(private http:Es3HttpClient, @Inject(SERVER_URL) private serverUrl) { }
+  private dataSource: DataSourceService<DisabilityType>;
+  constructor(http: Es3HttpClient, injector:Injector) { 
+    this.dataSource = new DataSourceService<DisabilityType>(http, injector, "disabilities");
+  }
 
-  get():Observable<Array<DisabilityType>>{
-    return this.http.Get(this.api).pipe(map(result => {
-      return <Array<DisabilityType>>result;
-    }));
+  gets():Observable<Array<DisabilityType>>{
+    return this.dataSource.gets();
   }
 }
